964b68ad8f9534a54f5941f4d5ed56e1b983b8ef
Hhoang-create <hhoang@bisonok.com>
2021-05-19 18:17:36 -0500

    else/if

--- module2/day5/in-class.js
+++ module2/day5/in-class.js
@@ -1,2 +1,22 @@
 var addPetButton = document.getElementById('add-pet-button')
 console.log(addPetButton)
+
+// boolean, conditionals, branching logic
+
+'1' == 1
+'true' == true
+
+var age = 21
+
+if (age>= 21) {
+        console.log("you can drink")}
+    else  { 
+        console.log("you cannot drink")
+}
+
+var myName = ""
+if(myName) {
+    console.log("Hello")
+} else {
+    console.log("No hello")
+}
